b863a8eb87feac3bef0dfa6555aace3d13328a75
Petr Stehlik <xstehl14@stud.fit.vutbr.cz>
2017-05-29 09:59:46 +0200

    Remove diacritics

--- docs/conf.py
+++ docs/conf.py
@@ -50,8 +50,8 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'Flow Data Storage'
-copyright = '2017, Petr Stehlík'
-author = 'Petr Stehlík'
+copyright = '2017, CESNET'
+author = 'Petr Stehlik'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
